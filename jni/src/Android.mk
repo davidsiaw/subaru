cc02a4d959437ae9c7b569cb40618d601648f557
David Siaw <davidsiaw@gmail.com>
2023-06-08 09:42:15 +0900

    add sdlnet

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -8,6 +8,7 @@ SDL_PATH := ../SDL
 SDL_image_PATH := ../SDL_image
 SDL_mixer_PATH := ../SDL_mixer
 SDL_ttf_PATH := ../SDL_ttf
+SDL_net_PATH := ../SDL_net
 BOOST_PATH := ../boost
 
 LUACPPINTERFACE_PATH := ../luacppinterface/LuaCppInterface
@@ -20,6 +21,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_image_PATH) \
 	$(LOCAL_PATH)/$(SDL_mixer_PATH) \
 	$(LOCAL_PATH)/$(SDL_ttf_PATH) \
+	$(LOCAL_PATH)/$(SDL_net_PATH) \
 	$(LOCAL_PATH)/$(LUACPPINTERFACE_PATH) \
 	$(LOCAL_PATH)/$(LUA_PATH)
 
@@ -31,7 +33,7 @@ LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) \
   $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
 	
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL_net
 LOCAL_STATIC_LIBRARIES := luacppinterface lua
 
 LOCAL_LDLIBS := -lGLESv1_CM -llog
